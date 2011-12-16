cacdda3c420f69f5e60cb00960adfa84bab132eb
Martijn van Exel <mvexel@gmail.com>
2011-12-16 15:35:03 -0700

    changed logic to retrieve points

--- service/gnis.py
+++ service/gnis.py
@@ -4,43 +4,50 @@ Created on Oct 17, 2011
 @author: mvexel
 '''
 
-import sys
 import web
 import psycopg2
-from random import sample
+from random import shuffle
 import simplejson as json
 
 urls = (
     '/(.*)/(.*)', 'getgnis'
 )
 
+
+done = 0
+
 app = web.application(urls, globals(), autoreload=False)
-session = web.session.Session(app, web.session.DiskStore('/tmp/sessions'), initializer={'seen': []})
+session = web.session.Session(app, web.session.DiskStore('/tmp/sessions'), initializer={'unseen': [], 'distances': [2^i/100 for i in range(0,5)], 'longitude': 0.0, 'latitude': 0.0})
 application = app.wsgifunc()
 
 class getgnis:
-    def GET(self, lon, lat):
-        if not lon and lat: 
-            return "no no"
+    def GET(self, longitude, latitude):
+        if longitude != session.longitude or latitude != session.latitude:
+            del session.unseen[:]
+        session.longitude = longitude
+        session.latitude = latitude
+        if len(session.unseen) > 0:
+            shuffle(session.unseen)
+            return json.dumps(session.unseen.pop())
+        else:
+            while len(session.unseen) == 0 and len(session.distances) > 0:
+                distance = session.distances.pop(0)
+                session.unseen = self.getgnispoints(distance, longitude, latitude)
+            if len(session.unseen) == 0:
+                return json.dumps([])
+            shuffle(session.unseen)
+            return json.dumps(session.unseen.pop())
+
+    def put(self, change):
+        pass
+
+    def getgnispoints(self, distance, longitude, latitude):
         conn = psycopg2.connect("host=localhost dbname=osm user=osm password=osm")
         cur = conn.cursor()
-        for i in (0.01,0.02,0.05,0.1):
-            strBox = '\'BOX(%f %f, %f %f)\'::box2d' % (float(lon)-i, float(lat)-i, float(lon)+i, float(lat)+i)
-            sys.stderr.write(strBox)
-            querysql = "SELECT id, tags->'name',tags->'gnis:Class',ST_X(geom),ST_Y(geom) FROM nodes WHERE geom && %s AND tags ? 'gnis:id' AND version <= 2" % (strBox)
-            sys.stderr.write(querysql)
-            cur.execute(querysql)
-            recs = cur.fetchall()
-            if len(recs) > 0:
-                rec = sample(recs,1)
-                if rec[0] in session.seen:
-                    continue
-                session.seen.append(rec[0])
-                return json.dumps(rec, len(session.seen))
-        return json.dumps([])
-
-def put(self, change):
-        pass
+        strBox = '\'BOX(%f %f, %f %f)\'::box2d' % (float(longitude)-distance/2, float(latitude)-distance/2, float(longitude)+distance/2, float(latitude)+distance/2)
+        querysql = "SELECT id, tags->'name',tags->'gnis:Class',ST_X(geom),ST_Y(geom) FROM nodes WHERE geom && %s AND tags ? 'gnis:id' AND version <= 2" % (strBox)
+        cur.execute(querysql)
+        return cur.fetchall()
 
 if __name__ == "__main__":
     app.run()
